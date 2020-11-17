15cb16cddbc8f98c0dca973e6dfd75a8e21bb4a0
Andreas Rosland <andreas@ros.land>
2020-11-17 08:40:46 -0500

    higer acuracy on frac_met_surface

--- pscr_loop.py
+++ pscr_loop.py
@@ -12,7 +12,8 @@ Frac_freshgas * ( (1/10) * MV * Frac_met_surface) + ( Frac_loop - (Frac_met_surf
 Frac_freshgas = Gas from cylinder in fraction of O2 (eg, 0.32, 0.36, 0.40) 
 MV = Minute Volume (SCR rate… 20l, 15l etc)
 P = Total pressure at relevant depth ATA + Hydrostatic pressure
-Frac_met_surface = Fraction of o2 metabolism on surface (ATA = 1), O2 consumption per min/MV. Eg 0.8l/20l = 0.04 or use static value 0.042
+Frac_met_surface = Fraction of o2 metabolism on surface (ATA = 1), O2 consumption per min/MV. 
+                   Eg; (0.8l/20l) = 0.04 or use the static value 0.042
 Frac_loop = Fraction of oxygen in RB loop (the result of this equation) FiO2
 
 It looks like the first formula given doesnt correct for depth so i added P to the numerator, 
@@ -24,7 +25,7 @@ mv = float(input("Minute volume: "))
 presure = float(input("Pressure at depth: "))
 bellow = int(input("Bellow ratio (6-10): "))
 
-frac_met_surface = 0.04
+frac_met_surface = 0.042
 new_gas_fraction = 1/bellow
 old_gas_fraction = (bellow -1)/bellow
 frac_loop = frac_freshgas
