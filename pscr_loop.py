368aec672d4c270d487124ef9c145526c85fba0f
Andreas Rosland <andreas@ros.land>
2020-11-17 18:56:18 -0500

    print help if no arguments is given

--- pscr_loop.py
+++ pscr_loop.py
@@ -12,6 +12,7 @@ Frac_met_surface = Fraction of o2 metabolism on surface (ATA = 1), O2 consumptio
 Frac_loop = Fraction of oxygen in RB loop (the result of this equation) FiO2
 """
 import argparse
+import sys
 
 def calc_loop(frac_freshgas, mv, presure, bellow):
     frac_met_surface =  (0.8/mv) # good static value 0.042
@@ -29,7 +30,7 @@ def calc_loop(frac_freshgas, mv, presure, bellow):
     return frac_loop
 
 def run(args):
-    frac_freshgas = args.o2fraction
+    frac_freshgas = args.fractionoxy
     mv = args.mv
     depth = args.depth
     bellow = args.bellow
@@ -42,11 +43,14 @@ def run(args):
 
 def main():
     parser=argparse.ArgumentParser(description="Calculate oxygen fraction in loop")
-    parser.add_argument("-f","--o2fraction",help="Oxygen fraction of breathing gas on cylinder" ,dest="o2fraction", type=float, required=True)
-    parser.add_argument("-d","--depth",help="Depth you calculate for in meters" ,dest="depth", type=float, default=0 , required=False)
+    parser.add_argument("-f","--fractionoxy",help="Oxygen fraction of breathing gas on cylinder" ,dest="fractionoxy", type=float, required=True)
+    parser.add_argument("-d","--depth",help="The depth you calculate for in meters(m)" ,dest="depth", type=float, default=0 , required=False)
     parser.add_argument("-v","--minutevolume",help="Minute Volume, liters you breath in one minute" ,dest="mv", type=float, default=19 , required=False)
     parser.add_argument("-b","--bellowratio",help="Ratio of bellow replacement rate 1:6 to 1:10" ,dest="bellow", type=int, default=10 , required=False)
     parser.set_defaults(func=run)
+    if len(sys.argv)==1:
+        parser.print_help(sys.stderr)
+        sys.exit(1)
     args=parser.parse_args()
     args.func(args)
 
