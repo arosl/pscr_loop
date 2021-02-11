0370b56e6599bab14f19ba94846afff852c8390a
Andreas Rosland <andreas@ros.land>
2021-02-10 22:37:24 -0500

    change variable names to better match example

--- pscr_loop.py
+++ pscr_loop.py
@@ -8,7 +8,7 @@ Fmix = Gas from cylinder in fraction of O2 (eg, 0.32, 0.36, 0.40)
 Ve = Minute Volume/RMV ex (SCR rate… 20l, 15l )
 Vo2 = Volume O2L/min ex (0.8l)
 Pamb = Total ambient pressure at relevant depth ex 1.6ata 
-Kratio = bellow ratio fraction ex (1/10 1/6)
+Kratio = Kratio ratio fraction ex (1/10 1/6)
 Ke = Ve/Vo2 ex (0.8l/20l) = 0.04 or use the static value 0.042
 Frac_loop = Fraction of oxygen in RB loop (the result of this equation) FiO2
 
@@ -40,72 +40,74 @@ from distutils.util import strtobool
 
 
 # Caclulate oxygen fraction in the breathing loop
-def calc_loop(frac_freshgas, mv, presure, bellow):
-    frac_met_surface = (0.8/mv)  # good static value 0.042
-    new_gas_fraction = 1/bellow
-    old_gas_fraction = (bellow - 1)/bellow
-    frac_loop = frac_freshgas
+def calc_loop(Fmix, Ve, Pamb, Kratio):
+    frac_met_surface = (0.8/Ve)  # good static value 0.042
+    new_gas_fraction = 1/Kratio
+    old_gas_fraction = (Kratio - 1)/Kratio
+    frac_loop = Fmix
     old_loop = frac_loop + 1
     o2_drop = []
 
     while (old_loop - frac_loop) > 0.000000001:
         old_loop = frac_loop
         #formula from the top of the file
-        frac_loop = (frac_freshgas * (new_gas_fraction * mv * presure +
-        (1 - new_gas_fraction) * mv * frac_met_surface) + (frac_loop -
-        (frac_met_surface/presure)) * (old_gas_fraction*mv*presure)
-        )/(mv * presure)
+        frac_loop = (Fmix * (new_gas_fraction * Ve * Pamb +
+        (1 - new_gas_fraction) * Ve * frac_met_surface) + (frac_loop -
+        (frac_met_surface/Pamb)) * (old_gas_fraction*Ve*Pamb)
+        )/(Ve * Pamb)
         
         o2_drop.append(frac_loop)
     return o2_drop
 
+
+
 # Cacluate the minumim and maximum safe area for a given oxygen fraction
-def min_max_gas(frac_freshgas, mv, presure, bellow, deco):
+def min_max_gas(Fmix, Ve, Pamb, Kratio, deco):
     FiO2 = 0
-    presure = float(1)
+    Pamb = float(1)
     o2_min = 0
     pp02lim = 1.3
     if deco:
         pp02lim = 1.6
 
-    while (FiO2 * presure) <= pp02lim:
-        o2drop = calc_loop(frac_freshgas, mv, presure, bellow)
+    while (FiO2 * Pamb) <= pp02lim:
+        o2drop = calc_loop(Fmix, Ve, Pamb, Kratio)
         FiO2 = o2drop[-1]
-        ppO2 = FiO2 * presure
-        depth = (presure - 1)*10
+        ppO2 = FiO2 * Pamb
+        depth = (Pamb - 1)*10
         if ppO2 < 0.21:
             o2_min = depth
         #set o2_max 1 meter deeper than current ppO2lim
-        o2_max = (presure - 0.9)*10
-        presure = round((presure + 0.1), 2)
+        o2_max = (Pamb - 0.9)*10
+        Pamb = round((Pamb + 0.1), 2)
 
     return(o2_min, o2_max)
 
 def run(args):
-    frac_freshgas = args.fractionoxy
-    mv = args.mv
+    Fmix = args.fractionoxy
+    Ve = args.Ve
     depth = args.depth
-    bellow = args.bellow
+    Kratio = args.Kratio
     graph = args.graph
     no_min_max = args.nominmax
     noppo2 = args.nopp02
     nofio2 = args.nofi02
     deco = args.deco
-    presure = (depth/10) + 1
+    Pamb = (depth/10) + 1
 
-    if frac_freshgas > 100:
+    if Fmix > 100:
         sys.exit(1)
-    if frac_freshgas <= 0:
+    if Fmix <= 0:
         sys.exit(1)
-    if frac_freshgas > 1:
-        frac_freshgas = frac_freshgas/100
+    if Fmix > 1:
+        Fmix = Fmix/100
 
-    o2drop = calc_loop(frac_freshgas, mv, presure, bellow)
+    o2drop = calc_loop(Fmix, Ve, Pamb, Kratio)
 
     if graph:
         label = """for O2 fraction %.2f at depth %.0dm
 level of at FiO2 %.2f, ppO2 %.2f 
-""" % (frac_freshgas, depth, o2drop[-1], (o2drop[-1]*presure))
+""" % (Fmix, depth, o2drop[-1], (o2drop[-1]*Pamb))
 
         x = range(0, len(o2drop))
         y = o2drop
@@ -116,10 +118,10 @@ level of at FiO2 %.2f, ppO2 %.2f
     if not nofio2:
         print("FiO2 %.2f" % o2drop[-1])
     if not noppo2:
-        print("ppO2 %.2f" % (o2drop[-1]*presure))
+        print("ppO2 %.2f" % (o2drop[-1]*Pamb))
     
     if not no_min_max:
-        o2_min_max = min_max_gas(frac_freshgas, mv, presure, bellow, deco)
+        o2_min_max = min_max_gas(Fmix, Ve, Pamb, Kratio, deco)
         o2_min = o2_min_max[0]
         if (o2_min%3) != 0:
             o2_min_table = o2_min + (3 - o2_min%3)
@@ -146,10 +148,10 @@ def main():
         dest="depth", type=float, default=0, required=False)
     parser.add_argument("-v", "--minutevolume",
         help="Minute Volume, liters you breath in one minute",
-        dest="mv", type=float, default=19, required=False)
+        dest="Ve", type=float, default=19, required=False)
     parser.add_argument("-b", "--bellowratio",
         help="Ratio of bellow replacement rate 1:6 to 1:10",
-        dest="bellow", type=int, default=10, required=False)
+        dest="Kratio", type=int, default=10, required=False)
     parser.add_argument("-g", "--graph", help="Print a graph of oxygen drop",
         dest="graph", type=lambda x: bool(strtobool(x)),
         nargs='?', const=True, default=False)
